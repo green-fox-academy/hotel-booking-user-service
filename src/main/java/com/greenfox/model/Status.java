79952f9120a866fde89da197990190fe698691ca
zsiguli <markovichzsigmond@gmail.com>
2017-06-09 12:18:25 +0200

    fix(Status): select id for entity

--- src/main/java/com/greenfox/model/Status.java
+++ src/main/java/com/greenfox/model/Status.java
@@ -1,12 +1,14 @@
 package com.greenfox.model;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "hearthbeat")
 public class Status {
 
+  @Id
   private Boolean status;
 
   public Boolean getStatus() {
