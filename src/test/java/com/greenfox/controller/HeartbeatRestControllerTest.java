a0e722677d2a693dcf17f3fb81c08b6523824893
zsiguli <markovichzsigmond@gmail.com>
2017-06-09 17:01:57 +0200

    fix(testDB): replace @DataJpaTest annotation

--- src/test/java/com/greenfox/controller/HeartbeatRestControllerTest.java
+++ src/test/java/com/greenfox/controller/HeartbeatRestControllerTest.java
@@ -7,6 +7,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -23,6 +24,7 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppC
 @SpringBootTest(classes = UserServiceApplication.class)
 @WebAppConfiguration
 @EnableWebMvc
+@DataJpaTest
 public class HeartbeatRestControllerTest {
 
   private MockMvc mockMvc;
