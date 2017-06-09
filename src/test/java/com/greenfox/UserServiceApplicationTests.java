8cb4b40f20b5b80d155b58500808252192dd4a85
zsiguli <markovichzsigmond@gmail.com>
2017-06-09 16:54:35 +0200

    feat(tests): use @DataJpaTest

--- src/test/java/com/greenfox/UserServiceApplicationTests.java
+++ src/test/java/com/greenfox/UserServiceApplicationTests.java
@@ -2,15 +2,17 @@ package com.greenfox;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@DataJpaTest
 public class UserServiceApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+  @Test
+  public void contextLoads() {
+  }
 
 }
