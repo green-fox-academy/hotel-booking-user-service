0279cabd428a15c221ac735b05deca1c45932bff
zsiguli <markovichzsigmond@gmail.com>
2017-06-09 12:57:23 +0200

    fix(heartbeat): respons properly without database

--- src/main/java/com/greenfox/controller/HearthbeatRestController.java
+++ src/main/java/com/greenfox/controller/HearthbeatRestController.java
@@ -4,6 +4,7 @@ import com.greenfox.model.Hearthbeat;
 import com.greenfox.repository.HearthbeatRepository;
 import com.greenfox.service.HearthbeatService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,11 @@ public class HearthbeatRestController {
   public HearthbeatRestController(HearthbeatService hearthbeatService, HearthbeatRepository hearthbeatRepository) {
     this.hearthbeatService = hearthbeatService;
     this.hearthbeatRepository = hearthbeatRepository;
+  }
+
+  @ExceptionHandler(Exception.class)
+  public Hearthbeat databaseError() {
+    return new Hearthbeat("ok");
   }
 
   @GetMapping("/hearthbeat")
--- src/main/java/com/greenfox/service/HearthbeatService.java
+++ src/main/java/com/greenfox/service/HearthbeatService.java
@@ -8,9 +8,7 @@ import org.springframework.stereotype.Service;
 public class HearthbeatService {
 
   public Hearthbeat getHearthBeat(HearthbeatRepository hearthbeatRepository) {
-    if (hearthbeatRepository == null) {
-      return new Hearthbeat("ok");
-    } else if (hearthbeatRepository.count() == 0) {
+    if (hearthbeatRepository.count() == 0) {
       return new Hearthbeat("ok","error");
     } else {
       return new Hearthbeat("ok", "ok");
