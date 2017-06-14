9256b114c4fda60b838a3ef11f8456a867f0e48b
Dombovari Peter <dombo.peter@gmail.com>
2017-06-14 14:50:39 +0200

    create logging using log4j, set log level trough env variable, correct typo

--- src/main/java/com/greenfox/controller/HearthbeatRestController.java
+++ src/main/java/com/greenfox/controller/HearthbeatRestController.java
@@ -3,16 +3,18 @@ package com.greenfox.controller;
 import com.greenfox.model.Hearthbeat;
 import com.greenfox.repository.HearthbeatRepository;
 import com.greenfox.service.HearthbeatService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HearthbeatRestController {
-
-  HearthbeatService hearthbeatService;
-  HearthbeatRepository hearthbeatRepository;
+  private final static Logger LOGGER = LoggerFactory.getLogger(HearthbeatRestController.class.getName());
+  private HearthbeatService hearthbeatService;
+  private HearthbeatRepository hearthbeatRepository;
 
   @Autowired
   public HearthbeatRestController(HearthbeatService hearthbeatService, HearthbeatRepository hearthbeatRepository) {
@@ -20,13 +22,13 @@ public class HearthbeatRestController {
     this.hearthbeatRepository = hearthbeatRepository;
   }
 
-  @ExceptionHandler(Exception.class)
-  public Hearthbeat databaseError() {
-    return new Hearthbeat("ok");
-  }
-
   @GetMapping("/hearthbeat")
   public Hearthbeat validateMessage() {
+    LOGGER.trace("trace message");
+    LOGGER.debug("debug message");
+    LOGGER.info("info message");
+    LOGGER.warn("warn message");
+    LOGGER.error("error message");
     return hearthbeatService.getHearthBeat(hearthbeatRepository);
   }
 }
