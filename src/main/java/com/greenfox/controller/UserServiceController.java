ef472e6d7fcae2b724b2a4e9e8421b16ef4b14a0
Dombovari Peter <dombo.peter@gmail.com>
2017-06-21 12:49:32 +0200

    update /register endpoing: create new Account with posted informations

--- src/main/java/com/greenfox/controller/UserServiceController.java
+++ src/main/java/com/greenfox/controller/UserServiceController.java
@@ -1,13 +1,19 @@
 package com.greenfox.controller;
 
+import com.greenfox.model.Account;
+import com.greenfox.model.Data;
+import com.greenfox.model.RequestData;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserServiceController {
 
   @PostMapping("/register")
-  public void newuser() {
-
+  public Account saveAccount(@RequestBody RequestData data) {
+    String email = data.getData().getAttributes().getEmail();
+    String password = data.getData().getAttributes().getPassword();
+    return (new Account(email,password));
   }
 }
