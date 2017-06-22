c6afa8ea629d721a4c1f49f486b9212fb84189c6
Dombovari Peter <dombo.peter@gmail.com>
2017-06-22 16:07:43 +0200

    Create AccountRepository to get user infos from DB (id,admin,email,token);

--- src/main/java/com/greenfox/repository/AccountRepository.java
+++ src/main/java/com/greenfox/repository/AccountRepository.java
@@ -1,5 +1,8 @@
 package com.greenfox.repository;
 
-public interface AccountRepository {
+import com.greenfox.model.Account;
+import org.springframework.data.repository.CrudRepository;
 
+public interface AccountRepository extends CrudRepository<Account, Long> {
+  public Account findAccountByEmail(String email);
 }
