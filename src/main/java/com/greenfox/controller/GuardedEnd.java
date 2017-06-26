9962aa3386c84b87f7745c3b475ff8751823efde
zsiguli <markovichzsigmond@gmail.com>
2017-06-26 11:35:36 +0200

    feat(guardedEnd): add new endpoint

---
+++ src/main/java/com/greenfox/controller/GuardedEnd.java
@@ -0,0 +1,26 @@
+package com.greenfox.controller;
+
+import com.greenfox.model.Error;
+import com.greenfox.model.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class GuardedEnd {
+
+  @GetMapping("/user/{id}")
+  public ResponseEntity getUserRelatedStuff() {
+    List tempList = new ArrayList();
+    Error tempError = new Error("401",
+            "Unauthorized",
+            "No token is provided");
+    tempList.add(tempError);
+    ErrorResponse tempResp = new ErrorResponse(tempList);
+    return new ResponseEntity(tempResp, HttpStatus.UNAUTHORIZED);
+  }
+}
