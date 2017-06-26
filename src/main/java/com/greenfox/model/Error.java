3378c0f4cd987800574f27c79e54b688c4eb98c9
zsiguli <markovichzsigmond@gmail.com>
2017-06-26 10:53:54 +0200

    chore(Error): add Error model

---
+++ src/main/java/com/greenfox/model/Error.java
@@ -0,0 +1,26 @@
+package com.greenfox.model;
+
+public class Error {
+
+  private String status;
+  private String title;
+  private String detail;
+
+  public Error(String status, String title, String detail) {
+    this.status = status;
+    this.title = title;
+    this.detail = detail;
+  }
+
+  public String getStatus() {
+    return status;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public String getDetail() {
+    return detail;
+  }
+}
