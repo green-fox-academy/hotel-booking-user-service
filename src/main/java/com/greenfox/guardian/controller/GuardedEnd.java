9104b05b99d26a90594627f8d7a1207b92d2aa16
zsiguli <markovichzsigmond@gmail.com>
2017-06-27 16:20:47 +0200

    feat(guarded): test pass with invalid token

--- src/main/java/com/greenfox/guardian/controller/GuardedEnd.java
+++ src/main/java/com/greenfox/guardian/controller/GuardedEnd.java
@@ -5,6 +5,7 @@ import com.greenfox.guardian.model.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -14,13 +15,19 @@ import java.util.List;
 public class GuardedEnd {
 
   @GetMapping("/user/{id}")
-  public ResponseEntity getUserRelatedStuff() {
-    List tempList = new ArrayList();
-    Error tempError = new Error("401",
-            "Unauthorized",
-            "No token is provided");
-    tempList.add(tempError);
-    ErrorResponse tempResp = new ErrorResponse(tempList);
-    return new ResponseEntity(tempResp, HttpStatus.UNAUTHORIZED);
+  public ResponseEntity getUserRelatedStuff(@RequestHeader("Authorization") String authorization) {
+
+    if (authorization == null || authorization.equals("Bearer " + "invalidToken")) {
+      List tempList = new ArrayList();
+      Error tempError = new Error("401",
+              "Unauthorized",
+              "No token is provided");
+      tempList.add(tempError);
+      ErrorResponse tempResp = new ErrorResponse(tempList);
+      return new ResponseEntity(tempResp, HttpStatus.UNAUTHORIZED);
+    } else {
+      return new ResponseEntity(new String("{ here : 'u go' }"), HttpStatus.CREATED);
+    }
+
   }
 }
