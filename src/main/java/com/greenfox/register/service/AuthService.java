cd27e19f8921a0c70ba43bc93a847aa5725b3929
Dombovari Peter <dombo.peter@gmail.com>
2017-06-28 15:22:11 +0200

    feat(register,login): create Services for authentication and json handling

--- src/main/java/com/greenfox/register/service/AuthService.java
+++ src/main/java/com/greenfox/register/service/AuthService.java
@@ -1,5 +1,36 @@
 package com.greenfox.register.service;
 
+import com.greenfox.register.exception.InvalidPasswordException;
+import com.greenfox.register.exception.NoSuchAccountException;
+import com.greenfox.register.model.Account;
+import com.greenfox.register.repository.AccountRepository;
+import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AuthService {
 
+  @Autowired
+  AccountRepository accountRepository;
+
+  public void authenticate(String email, String password) throws Exception {
+    Account account;
+    if (checkAccount(email)) {
+      throw new NoSuchAccountException("Invalid email");
+    } else {
+      account = accountRepository.findAccountByEmail(email);
+    }
+    if (!checkPassword(password, account.getPassword())) {
+      throw new InvalidPasswordException("Invalid password");
+    };
+  }
+
+  public boolean checkAccount(String email) {
+    return (accountRepository.findAccountByEmail(email) == null);
+  }
+
+  public boolean checkPassword(String password, String pw_hashed) {
+    return BCrypt.checkpw(password, pw_hashed);
+  }
 }
--- src/main/java/com/greenfox/register/service/GsonService.java
+++ src/main/java/com/greenfox/register/service/GsonService.java
@@ -1,5 +1,37 @@
 package com.greenfox.register.service;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonPrimitive;
+import com.greenfox.register.model.Credentials;
+import org.springframework.stereotype.Service;
+
+@Service
 public class GsonService {
 
+  public Credentials parseCredentials(String json) {
+    JsonElement jelement = new JsonParser().parse(json);
+    JsonObject data = jelement.getAsJsonObject();
+    data = data.getAsJsonObject("data");
+    JsonObject attributes = data.getAsJsonObject("attributes");
+    String email = attributes.get("email").getAsString();
+    String password = attributes.get("password").getAsString();
+    return new Credentials(email,password);
+  }
+
+  public String createAccountJson(Long id, String email, boolean admin, String token) {
+    JsonObject jobject = new JsonObject();
+    JsonObject data = new JsonObject();
+    JsonObject attributes = new JsonObject();
+    jobject.add("data", data);
+    data.addProperty("type","user");
+    data.add("attributes", attributes);
+    attributes.addProperty("id", id.toString());
+    attributes.addProperty("email",email);
+    System.out.println(email);
+    attributes.addProperty("admin",admin);
+    attributes.addProperty("token",token);
+    return jobject.toString();
+  }
 }
