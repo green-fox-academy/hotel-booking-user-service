1fcf8c7c9da31887769a20d6b3640b98a04c76ff
Dombovari Peter <dombo.peter@gmail.com>
2017-06-28 15:20:15 +0200

    feat(register,login): create Credentials for parsing json email and password fields;

--- src/main/java/com/greenfox/register/model/Credentials.java
+++ src/main/java/com/greenfox/register/model/Credentials.java
@@ -1,5 +1,19 @@
 package com.greenfox.register.model;
 
 public class Credentials {
+  private String email;
+  private String password;
 
+  public Credentials(String email, String password) {
+    this.email = email;
+    this.password = password;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
 }
