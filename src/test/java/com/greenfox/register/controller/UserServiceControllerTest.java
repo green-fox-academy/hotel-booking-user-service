49e49458c7de9d9c53c9cb8913b1d5591952a432
Dombovari Peter <dombo.peter@gmail.com>
2017-06-28 13:46:03 +0200

    test(register, login): write integration tests for register and login endpoint for new, already registered and authorized, unauthorized user

--- src/test/java/com/greenfox/register/controller/UserServiceControllerTest.java
+++ src/test/java/com/greenfox/register/controller/UserServiceControllerTest.java
@@ -2,16 +2,25 @@ package com.greenfox.register.controller;
 
 import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 import com.google.gson.JsonObject;
 import com.greenfox.UserServiceApplication;
+import com.greenfox.register.model.Account;
+import com.greenfox.register.repository.AccountRepository;
+import com.greenfox.register.service.JwtCreator;
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -25,10 +34,17 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @WebAppConfiguration
 @EnableWebMvc
 @ActiveProfiles("test")
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class UserServiceControllerTest {
 
   private MockMvc mockMvc;
   private String jsonInput;
+
+  @Autowired
+  private JwtCreator jwtCreator;
+
+  @Autowired
+  private AccountRepository accountRepository;
 
   @Autowired
   private WebApplicationContext webApplicationContext;
@@ -36,26 +52,73 @@ public class UserServiceControllerTest {
   @Before
   public void setup() throws Exception {
     mockMvc = webAppContextSetup(webApplicationContext).build();
-    JsonObject jobject = new JsonObject();
-    JsonObject data = new JsonObject();
-    JsonObject attributes = new JsonObject();
-    jobject.add("data",data);
-    data.addProperty("type","user");
-    data.add("attributes", attributes);
-    attributes.addProperty("email", "dombo.peter@example.com");
-    attributes.addProperty("password","suchsecret");
-    jsonInput = jobject.toString();
+    jsonInput = jsonForRegister();
   }
 
   @Test
-  public void saveAccountToDatabase() throws Exception {
+  public void AAA_saveNewAccountToDatabase_ExpectCreated() throws Exception {
+    accountRepository.deleteAll();
+    mockMvc.perform(post("/register")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(jsonInput))
+        .andExpect(status().isCreated())
+        .andExpect(jsonPath("$.data.type").value("user"))
+        .andExpect(jsonPath("$.data.attributes.id").value(1))
+        .andExpect(jsonPath("$.data.attributes.email").value("john.doe@example.org"))
+        .andExpect(jsonPath("$.data.attributes.admin").value(false))
+        .andExpect(jsonPath("$.data.attributes.token").isNotEmpty());
+  }
+
+  @Test
+  public void saveAlreadyRegisteredAccountToDatabase_ExpectConflict() throws Exception {
+    accountRepository.deleteAll();
+    accountRepository.save(new Account("john.doe@example.org",false,"token","suchsecret"));
     mockMvc.perform(post("/register")
         .contentType(MediaType.APPLICATION_JSON)
         .content(jsonInput))
         .andExpect(status().isConflict());
   }
 
+
+
   @Test
-  public void authenticateAccount() throws Exception {
+  public void AAB_loginAuthenticadedUser_ExpectOK() throws Exception {
+    accountRepository.deleteAll();
+    String pw_hashed = BCrypt
+        .hashpw("suchsecret", BCrypt.gensalt((Integer.parseInt(System.getenv("LOG_ROUNDS")))));
+    accountRepository.save(new Account("john.doe@example.org",false,"token",pw_hashed));
+    mockMvc.perform(post("/login")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(jsonInput))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.data.type").value("user"))
+        .andExpect(jsonPath("$.data.attributes.id").value(2))
+        .andExpect(jsonPath("$.data.attributes.email").value("john.doe@example.org"))
+        .andExpect(jsonPath("$.data.attributes.admin").value(false))
+        .andExpect(jsonPath("$.data.attributes.token").isNotEmpty());
+  }
+
+  @Test
+  public void AAC_loginUnAuthorizedUser_ExpectUnauth() throws Exception {
+    accountRepository.deleteAll();
+    String pw_hashed = BCrypt
+        .hashpw("WRONGsecret", BCrypt.gensalt((Integer.parseInt(System.getenv("LOG_ROUNDS")))));
+    accountRepository.save(new Account("john.doe@example.org",false,"token",pw_hashed));
+    mockMvc.perform(post("/login")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(jsonInput))
+        .andExpect(status().isUnauthorized());
+  }
+
+  public String jsonForRegister() {
+    JsonObject jobject = new JsonObject();
+    JsonObject data = new JsonObject();
+    JsonObject attributes = new JsonObject();
+    jobject.add("data", data);
+    data.addProperty("type","user");
+    data.add("attributes", attributes);
+    attributes.addProperty("email", "john.doe@example.org");
+    attributes.addProperty("password","suchsecret");
+    return jobject.toString();
   }
 }
