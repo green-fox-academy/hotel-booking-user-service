76497d7d7e0c4019e8f4fef0f3a891627cc81402
zsiguli <markovichzsigmond@gmail.com>
2017-07-03 11:15:56 +0200

    add PagingAndSortingRepository

--- src/main/java/com/greenfox/register/repository/AccountRepository.java
+++ src/main/java/com/greenfox/register/repository/AccountRepository.java
@@ -1,8 +1,8 @@
 package com.greenfox.register.repository;
 
 import com.greenfox.register.model.Account;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface AccountRepository extends CrudRepository<Account, Long> {
-  public Account findAccountByEmail(String email);
+public interface AccountRepository extends PagingAndSortingRepository<Account, Long> {
+  Account findAccountByEmail(String email);
 }
