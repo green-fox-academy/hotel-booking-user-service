1f1423b41d23e9019c470567c51d44c1c61444e3
zsiguli <markovichzsigmond@gmail.com>
2017-07-04 17:31:01 +0200

    feat(Checkout): time is up we have to go home

--- src/main/java/com/greenfox/users/Controller.java
+++ src/main/java/com/greenfox/users/Controller.java
@@ -7,20 +7,17 @@ import com.greenfox.register.model.Data;
 import com.greenfox.register.model.RequestData;
 import com.greenfox.register.repository.AccountRepository;
 import com.greenfox.users.model.Links;
-import java.util.ArrayList;
-import java.util.Enumeration;
-import java.util.List;
-import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
 
 @RestController
 public class Controller {
@@ -116,4 +113,23 @@ public class Controller {
     }
   }
 
+  @PatchMapping (value = "/api/users/{userId}")
+  public ResponseEntity updateUser(@PathVariable(required = false) Long userId, HttpServletRequest request, @RequestBody RequestData requestData) {
+    if (!(accountRepository.findOneById(userId, new PageRequest(0, 1)).getContent().size() == 0)) {
+      System.out.println(requestData);
+      Account accountToUpdate = accountRepository.findOne(userId);
+      accountToUpdate.setId(userId + 1);
+      accountToUpdate.setAdmin(false);
+      accountRepository.save(accountToUpdate);
+      return new ResponseEntity<>("{}",HttpStatus.OK);
+    } else {
+      List<Error> tempList = new ArrayList<>();
+      Error tempError = new Error("404",
+              "Not Found",
+              "No users found by id: " + userId);
+      tempList.add(tempError);
+      ErrorResponse tempResp = new ErrorResponse(tempList);
+      return new ResponseEntity<>(tempResp, HttpStatus.NOT_FOUND);
+    }
+  }
 }
