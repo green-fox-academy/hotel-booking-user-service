3335ee35c94111b11e482d3e0f904a06ab0a1dfa
zsiguli <markovichzsigmond@gmail.com>
2017-07-04 10:27:29 +0200

    chore(RequestData): add new field

--- src/main/java/com/greenfox/users/model/Link.java
+++ src/main/java/com/greenfox/users/model/Link.java
@@ -11,4 +11,36 @@ public class Link {
   private String prev;
   @JsonInclude(JsonInclude.Include.NON_NULL)
   private String last;
+
+  public String getSelf() {
+    return self;
+  }
+
+  public void setSelf(String self) {
+    this.self = self;
+  }
+
+  public String getNext() {
+    return next;
+  }
+
+  public void setNext(String next) {
+    this.next = next;
+  }
+
+  public String getPrev() {
+    return prev;
+  }
+
+  public void setPrev(String prev) {
+    this.prev = prev;
+  }
+
+  public String getLast() {
+    return last;
+  }
+
+  public void setLast(String last) {
+    this.last = last;
+  }
 }
