bf892428ec84253749c9fc308136cb0714428ce6
VVarro <virag.varro@gmail.com>
2017-07-12 12:00:46 +0200

    refactor userservice

--- src/main/java/com/greenfox/users/UserCRUDController.java
+++ src/main/java/com/greenfox/users/UserCRUDController.java
@@ -7,23 +7,30 @@ import com.greenfox.register.model.Data;
 import com.greenfox.register.model.RequestData;
 import com.greenfox.register.repository.AccountRepository;
 import com.greenfox.users.model.Links;
+import com.greenfox.users.service.UserCrudService;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
-import java.util.Enumeration;
-import java.util.List;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserCRUDController {
 
-  private AccountRepository accountRepository;
-  private Page<Account> responsePage;
+  @Autowired
+  AccountRepository accountRepository;
+  Page<Account> responsePage;
+  UserCrudService userCrudService;
 
   @Autowired
   public UserCRUDController(AccountRepository accountRepository) {
@@ -31,11 +38,14 @@ public class UserCRUDController {
   }
 
   @GetMapping("/api/users")
-  public ResponseEntity returnUsers(@RequestParam(value = "page", required = false, defaultValue = "0") Integer page, @RequestParam(value = "admin", required = false) boolean admin,
+  public ResponseEntity returnUsers(
+      @RequestParam(value = "page", required = false, defaultValue = "0") Integer page,
+      @RequestParam(value = "admin", required = false) boolean admin,
       HttpServletRequest request) {
 
-    if (isAdminQuery(request)) {
-      responsePage = adminFilterService(admin, page);
+    if (userCrudService.isInQuery(request, "admin")) {
+      responsePage = userCrudService.adminFilterService(admin, page);
+      System.out.println(userCrudService.getQueryValue(request, "admin"));
     } else {
       responsePage = accountRepository.findAll(new PageRequest(page, 20));
     }
@@ -50,7 +60,8 @@ public class UserCRUDController {
       links.setLast(request.getRequestURL() + "?page=" + responsePage.getTotalPages());
     }
     if (responsePage.hasPrevious()) {
-      links.setPrev(request.getRequestURL() + (request.getQueryString().endsWith("page=1") ? "" : "?page=" + (page - 1)));
+      links.setPrev(request.getRequestURL() + (request.getQueryString().endsWith("page=1") ? ""
+          : "?page=" + (page - 1)));
     }
 
     Data data = new Data(links, "user", responsePage.getContent());
@@ -58,24 +69,10 @@ public class UserCRUDController {
     return new ResponseEntity<>(requestData, HttpStatus.OK);
   }
 
-  public Page<Account> adminFilterService(boolean admin, int page){
-    return accountRepository.findAllByAdmin(admin, new PageRequest(page, 2));
-  }
-
-  public boolean isAdminQuery(HttpServletRequest request) {
-    Enumeration parameterNames = request.getParameterNames();
-
-    while (parameterNames.hasMoreElements()) {
-      String parameterName = parameterNames.nextElement().toString();
-      if (parameterName.equals("admin")) {
-        return true;
-      }
-    }
-    return false;
-  }
 
   @GetMapping("/api/users/{userId}")
-  public ResponseEntity returnUser(@PathVariable(required = false) Long userId, HttpServletRequest request) {
+  public ResponseEntity returnUser(@PathVariable(required = false) Long userId,
+      HttpServletRequest request) {
     Links links = new Links();
 
     if (!(accountRepository.findOneById(userId, new PageRequest(0, 1)).getContent().size() == 0)) {
@@ -90,18 +87,21 @@ public class UserCRUDController {
       return new ResponseEntity<>(tempResp, HttpStatus.NOT_FOUND);
     }
 
-    links.setSelf(request.getRequestURL().toString() + (request.getQueryString() != null ? "?" + request.getQueryString() : ""));
+    links.setSelf(
+        request.getRequestURL().toString() + (request.getQueryString() != null ? "?" + request
+            .getQueryString() : ""));
 
     Data data = new Data(links, "user", responsePage.getContent());
     RequestData requestData = new RequestData(data);
     return new ResponseEntity<>(requestData, HttpStatus.OK);
   }
 
-  @DeleteMapping (value = "/api/users/{userId}", produces = "application/json")
-  public ResponseEntity deleteUser(@PathVariable(required = false) Long userId, HttpServletRequest request) {
-      if (!(accountRepository.findOneById(userId, new PageRequest(0, 1)).getContent().size() == 0)) {
+  @DeleteMapping(value = "/api/users/{userId}", produces = "application/json")
+  public ResponseEntity deleteUser(@PathVariable(required = false) Long userId,
+      HttpServletRequest request) {
+    if (!(accountRepository.findOneById(userId, new PageRequest(0, 1)).getContent().size() == 0)) {
       accountRepository.delete(userId);
-      return new ResponseEntity<>("{}",HttpStatus.OK);
+      return new ResponseEntity<>("{}", HttpStatus.OK);
     } else {
       List<Error> tempList = new ArrayList<>();
       Error tempError = new Error("404",
@@ -113,20 +113,21 @@ public class UserCRUDController {
     }
   }
 
-  @PatchMapping (value = "/api/users/{userId}")
-  public ResponseEntity updateUser(@PathVariable(required = false) Long userId, HttpServletRequest request, @RequestBody RequestData requestData) {
+  @PatchMapping(value = "/api/users/{userId}")
+  public ResponseEntity updateUser(@PathVariable(required = false) Long userId,
+      HttpServletRequest request, @RequestBody RequestData requestData) {
     if (!(accountRepository.findOneById(userId, new PageRequest(0, 1)).getContent().size() == 0)) {
       System.out.println(requestData);
       Account accountToUpdate = accountRepository.findOne(userId);
       accountToUpdate.setId(userId + 1);
       accountToUpdate.setAdmin(false);
       accountRepository.save(accountToUpdate);
-      return new ResponseEntity<>("{}",HttpStatus.OK);
+      return new ResponseEntity<>("{}", HttpStatus.OK);
     } else {
       List<Error> tempList = new ArrayList<>();
       Error tempError = new Error("404",
-              "Not Found",
-              "No users found by id: " + userId);
+          "Not Found",
+          "No users found by id: " + userId);
       tempList.add(tempError);
       ErrorResponse tempResp = new ErrorResponse(tempList);
       return new ResponseEntity<>(tempResp, HttpStatus.NOT_FOUND);
--- src/main/java/com/greenfox/users/service/UserCrudService.java
+++ src/main/java/com/greenfox/users/service/UserCrudService.java
@@ -1,5 +1,55 @@
 package com.greenfox.users.service;
 
+import com.greenfox.register.model.Account;
+import com.greenfox.register.repository.AccountRepository;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserCrudService {
+
+  @Autowired
+  private AccountRepository accountRepository;
+  private Page<Account> responsePage;
+
+  public Page<Account> adminFilterService(boolean admin, int page) {
+    return accountRepository.findAllByAdmin(admin, new PageRequest(page, 2));
+  }
+
+  public boolean isInQuery(HttpServletRequest request, String parameter) {
+    Enumeration parameterNames = request.getParameterNames();
+
+    while (parameterNames.hasMoreElements()) {
+      String parameterName = parameterNames.nextElement().toString();
+      if (parameterName.equals(parameter)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public List<String> getQuery(HttpServletRequest request) {
+    List<String> params = new ArrayList<>();
+    Enumeration parameterNames = request.getParameterNames();
+
+    while (parameterNames.hasMoreElements()) {
+      params.add(parameterNames.nextElement().toString());
+    }
+    return params;
+  }
+
+  public String getQueryValue(HttpServletRequest request, String parameter) {
+    if (isInQuery(request, parameter)) {
+      return request.getParameter(parameter);
+    } else {
+      return null;
+    }
+  }
 
 }
