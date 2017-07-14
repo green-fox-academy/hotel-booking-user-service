10e7dbe59154ddb6caf71b853ae986be9b9e077e
VVarro <virag.varro@gmail.com>
2017-07-14 11:59:25 +0200

    refactor rabbitmq package

--- src/main/java/com/greenfox/rabbitmq/model/Send.java
+++ src/main/java/com/greenfox/rabbitmq/model/Send.java
@@ -2,8 +2,6 @@ package com.greenfox.rabbitmq.model;
 
 import com.google.gson.Gson;
 import com.greenfox.rabbitmq.service.ConnectionSetter;
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
 
 public class Send {
 
@@ -12,21 +10,15 @@ public class Send {
 
   public void send(String message) throws Exception {
     ConnectionSetter connectionSetter = new ConnectionSetter();
-    connectionSetter.setFactory();
-    Connection connection = connectionSetter.getFactory().newConnection();
-    Channel channel = connection.createChannel();
-    channel.basicPublish("", "heartbeat", null, message.getBytes("UTF-8"));
+    connectionSetter.getChannel().basicPublish("", "heartbeat", null, message.getBytes("UTF-8"));
     System.out.println("Message sent: '" + message + "'");
   }
 
   public String dispatch(String hostname, String message) throws Exception {
     ConnectionSetter connectionSetter = new ConnectionSetter();
-    connectionSetter.setFactory();
-    Connection connection = connectionSetter.getFactory().newConnection();
-    Channel channel = connection.createChannel();
     Gson gson = new Gson();
     String eventJson = gson.toJson(new Event(hostname, message));
-    channel.basicPublish("", "events", null, eventJson.getBytes());
+    connectionSetter.getChannel().basicPublish("", "events", null, eventJson.getBytes());
     return eventJson;
   }
 
